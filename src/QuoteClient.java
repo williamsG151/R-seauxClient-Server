1d6d044f0a6b1d604d1af5470422e7c35c672340
williams <graw3301@sherbooke.ca>
2020-06-13 10:52:24 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 6e0b872c0a0f57067a503a8c6f80f6d8accd1f78.

--- src/QuoteClient.java
+++ src/QuoteClient.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
->>>>>>> master
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -50,11 +16,7 @@ public class QuoteClient {
         // send request
         byte[] buf = new byte[256];
         InetAddress address = InetAddress.getByName(args[0]);
-<<<<<<< HEAD
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
-=======
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 8080);
->>>>>>> master
         socket.send(packet);
 
         // get response
@@ -67,8 +29,4 @@ public class QuoteClient {
 
         socket.close();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
