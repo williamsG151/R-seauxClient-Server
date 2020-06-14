abb4bd96b1d3fd68dcdc83ca8973783798e24d35
Victor <vicfreve@gmail.com>
2020-06-14 13:15:26 -0400

    NewTransport layer

--- src/LinkLayer.java
+++ src/LinkLayer.java
@@ -1,9 +1,35 @@
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
 
 public class LinkLayer implements LayersCommunication {
-    @Override
-    public void send(byte[] buf) throws IOException {
 
+    LayersCommunication upwardLayer = new TransportLayer();
+
+    private DatagramSocket socket = null;
+    private int transmitedFiles = 0;
+    private int lostedFiles = 0;
+    private int receivedFiles = 0;
+    private int corruptedFiles = 0;
+
+    public LinkLayer() {
+        try {
+            socket = new DatagramSocket(TransportLayer.PORT);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void send(String IPadress, byte[] buf) throws IOException {
+          byte crc=   CRC32.calculateCRC32(buf);
+          transmitedFiles++;
+          byte[] allBytesArray = new byte[1+buf.length];
+            ByteBuffer bf = ByteBuffer.wrap(allBytesArray);
+            bf.put(crc,buf);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
     }
 
     @Override
--- src/TransportLayer.java
+++ src/TransportLayer.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,40 +7,35 @@ import java.util.List;
 public class TransportLayer implements LayersCommunication {
     LayersCommunication downLayer = new LinkLayer();
     private List<byte[]> packets = new ArrayList<>();
-    private static final int PACKET_NUMBER_BYTE_QUANTITY= 5;
+    private static final int PACKET_NUMBER_SIZE = 5;
     private static final int MAX_PACKET_SIZE = 200;
-    private static final int PORT = 25000;
+    public static final int PORT = 25000;
 
 
     @Override
-    public void send(byte[] buf) throws IOException {
-        byte[] IPadress = ApplicationLayer.getIPAdress(buf);
-        byte[] port = String.valueOf(PORT).getBytes();
-        byte[] fileName = ApplicationLayer.getFileName(buf);
-        byte[] data = ApplicationLayer.getData(buf);
+    public void send(String IPdestinataire,byte[] buf) throws IOException {
 
 
-        int quantity = (int) (Math.ceil(data.length/MAX_PACKET_SIZE)+1);
 
-        byte[] packetQuantity = numberToString(quantity,PACKET_NUMBER_BYTE_QUANTITY).getBytes();
+        int quantity = (int) (Math.ceil(buf.length/MAX_PACKET_SIZE)+1);
+
+        byte[] packetQuantity = numberToString(quantity, PACKET_NUMBER_SIZE).getBytes();
         byte[] header, body, allBytes;
         ByteBuffer byteBuffer;
 
         for (int i = 0 ; i<quantity;i++){
-            byte[] packetNumber = numberToString(i,PACKET_NUMBER_BYTE_QUANTITY).getBytes();
+            byte[] packetNumber = numberToString(i, PACKET_NUMBER_SIZE).getBytes();
 
             //Création du header
-            header =  new byte[IPadress.length+port.length+2*PACKET_NUMBER_BYTE_QUANTITY];
+            header =  new byte[2* PACKET_NUMBER_SIZE];
             byteBuffer = ByteBuffer.wrap(header);
-            byteBuffer.put(IPadress).put(port).put(packetNumber).put(packetQuantity);
+            byteBuffer.put(packetNumber).put(packetQuantity);
 
             //Création du body
-            if(i ==0 ){
-                body = fileName;
-            }else if (i==quantity){
-                body = Arrays.copyOfRange(data,(i-1)*MAX_PACKET_SIZE, data.length);
+            if (i==quantity){
+                body = Arrays.copyOfRange(buf,(i-1)*MAX_PACKET_SIZE, buf.length);
             }else{
-                body = Arrays.copyOfRange(data,(i-1)*MAX_PACKET_SIZE, (i-1)*MAX_PACKET_SIZE+MAX_PACKET_SIZE);
+                body = Arrays.copyOfRange(buf,(i-1)*MAX_PACKET_SIZE, (i-1)*MAX_PACKET_SIZE+MAX_PACKET_SIZE);
             }
 
             //On join les deux ensemble et on les stock dans la liste
@@ -50,9 +44,8 @@ public class TransportLayer implements LayersCommunication {
             byteBuffer.put(header).put(body);
             packets.add(allBytes);
         }
-        for (byte[] bytes:packets
-             ) {
-           downLayer.send(bytes);
+        for (byte[] bytes:packets) {
+           downLayer.send(IPdestinataire,bytes);
         }
     }
 
